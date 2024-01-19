d6442c6135bb2039b0ce3723923348d74bb96c21
Raul German <rgerman02@northshore.edu>
2024-01-19 02:53:38 -0500

    update src folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
+import MbtaAlertsPage from "./components/pages/mbtaAlerts";
 import Navbar from "./components/navbar";
 import LandingPage from "./components/pages/landingPage";
 import HomePage from "./components/pages/homePage";
@@ -27,6 +28,7 @@ const App = () => {
       <Navbar />
       <UserContext.Provider value={user}>
         <Routes>
+          <Route exact path="/mbtaAlerts" element={<MbtaAlertsPage />} />
           <Route exact path="/" element={<LandingPage />} />
           <Route exact path="/home" element={<HomePage />} />
           <Route exact path="/login" element={<Login />} />
